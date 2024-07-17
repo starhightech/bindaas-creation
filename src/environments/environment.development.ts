bc1fd1e94f2b7b6f5993328771ada147a86e9da5
Anil Chaudhari <imanilchaudhari@gmail.com>
2024-07-17 11:59:31 +0545

    Fix: new assets aded

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,4 @@ export const environment = {
   APP_ID: process.env['APP_ID'],
   API_URL: process.env['API_URL'],
   API_KEY: process.env['API_KEY'],
-  GOOGLE_MAPS_APIKEY: process.env['GOOGLE_MAPS_APIKEY'],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiKey: 'prodKey'
+  APP_ID: process.env['APP_ID'],
+  API_URL: process.env['API_URL'],
+  API_KEY: process.env['API_KEY'],
 };
